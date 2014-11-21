1c1a81570e2e330e186f5b7946139e6c163263cf
James McKay <jjm538@uowmail.edu.au>
2014-11-21 16:37:42 +1100

    Add comment with Anaconda urls

--- insideChrootActions.bash
+++ insideChrootActions.bash
@@ -15,3 +15,7 @@ unopkg add --shared /custom/libre_office_4.oxt
 
 glib-compile-schemas /usr/share/glib-2.0/schemas/
 
+# Anaconda
+# 32-bit: http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86.sh
+# 64-bit: http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh
+
